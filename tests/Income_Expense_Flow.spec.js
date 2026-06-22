9e77099f765accfeebe74ab7f34fa9f8d4485d60
rizkysepp <rizky8septian@gmal.com>
2026-06-22 17:27:39 +0700

    Update script Income & Expense

--- tests/Income_Expense_Flow.spec.js
+++ tests/Income_Expense_Flow.spec.js
@@ -46,6 +46,54 @@ if (match) {
     await page.locator('(//span[text()="Income & Expense"])[2]').click();
     await expect(page).toHaveURL(/income-expense/);
 
+    // Add Income
+    await page.getByText('Add Income').click();
+    await page.locator('//input[@placeholder="0"]').fill('1000');
+    await page.locator('select').selectOption('Bonus');
+    await expect(
+      page.locator('select')
+    ).toHaveValue('Bonus');
+    await page.locator('(//input[contains(@class,"px-3")])[2]').fill('Bonus from KPI')
+    await expect(
+      page.locator('(//input[contains(@class,"px-3")])[2]')
+    ).toHaveValue('Bonus from KPI');
+    await page.locator('(//input[contains(@class,"px-3")])[3]').fill('2026-01-30');
+    await page.locator('//button[text()="Add"]').click();
+
+    // Add Expense
+    await page.getByText('Add Expense').click();
+    await page.locator('//input[@placeholder="0"]').fill('5000');
+    await page.locator('select').selectOption('Transport');
+    await expect(
+      page.locator('select')
+    ).toHaveValue('Transport');
+    await page.locator('//input[@placeholder="Optional note"]').fill('Transportation cost for client meeting');
+    await expect(
+      page.locator('//input[@placeholder="Optional note"]')
+    ).toHaveValue('Transportation cost for client meeting');
+    await page.locator('(//input[contains(@class,"px-3")])[3]').fill('2026-03-29');
+    await page.locator('//button[text()="Add"]').click();
+
+    // Manage Categories Income
+    await page.getByText('Manage Categories').click();
+    await page.locator('//input[@placeholder="Enter name"]').fill('Side Job');
+    await expect(
+      page.locator('//input[@placeholder="Enter name"]')
+    ).toHaveValue('Side Job');
+    await page.locator('//button[text()="Income"]').click();
+    await page.locator('//button[text()="Add Category"]').click();
+
+    // Manage Categories Expense
+    await page.getByText('Manage Categories').click();
+    await page.locator('//input[@placeholder="Enter name"]').fill('Netflix Subscription');
+    await expect(
+      page.locator('//input[@placeholder="Enter name"]')
+    ).toHaveValue('Netflix Subscription');
+    await page.locator('//button[text()="Expense"]').click();
+    await page.locator('//button[text()="Add Category"]').click();
+
+
+
 
     await page.pause();
 });
