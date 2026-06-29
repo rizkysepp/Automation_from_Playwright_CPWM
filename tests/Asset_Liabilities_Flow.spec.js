9a4ee8f5b8b1ac341a6eb65aaf2bff983cfccdb2
rizkysepp <rizky8septian@gmal.com>
2026-06-29 17:40:22 +0700

    Update IPO script & Added Asset Liabilities script

--- tests/Asset_Liabilities_Flow.spec.js
+++ tests/Asset_Liabilities_Flow.spec.js
@@ -1,6 +1,6 @@
 import {test, expect} from '@playwright/test';
 
-test('IPO flow', async ({page}) => {
+test('Income & Expense flow', async ({page}) => {
     await page.goto('https://initial-inquiry-pjs4.bolt.host/login');
     await expect(page.locator('h2')).toHaveText('CPWM');
 
@@ -42,22 +42,11 @@ if (match) {
 }
     await page.getByText('Login').click();
     await expect(page).toHaveURL(/dashboard/);  
-    await page.locator('(//a[@href="/ipo"])[2]').click();
-    await expect(page).toHaveURL(/ipo/);
 
-    //Subs IPO
-    await page.locator('(//button[text()="Details"])[1]').click();
-    await page.locator('(//button[text()="Subscribe to IPO"])[1]').click();
-    await expect(page.locator('h2')).toHaveText('IPO Subscription Successful!');
-    await page.locator('//button[text()="Done"]').click();
-
-    //Turn On Notification
-    await page.locator('(//button[contains(@class,"inline-flex")])[3]').click();
-
-    //Search
-    await page.locator('//input[@placeholder="Search IPO by company, ticker, or sector..."]').fill('Green Energy');
-    
-
+    await page.locator('(//span[text()="Assets & Liabilities"])[2]').click();
+    await expect(
+        page.locator('(//span[text()="Assets & Liabilities"])[2]')
+    ).toHaveText('Assets & Liabilities')
 
     await page.pause();
 
